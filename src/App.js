8c7587a8b803575f66d4d14e3f30d7330bdc2070
bacnh7427 <bacnh7427@co-well.com.vn>
2023-04-13 15:59:15 +0700

    add toggle todolist, using state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import TodoFeature from './features/Todo';
 import AlbumFeature from './features/Album';
+import ColorBox from './components/ColorBox';
+import Counter from './components/Counter';
 
 function App() {
     return (
     <div className="App">
       <header className="App-header">
+          <ColorBox />
+          <Counter />
           <TodoFeature />
           <AlbumFeature />
       </header>
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,38 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import TodoList from './components/TodoList';
-
 TodoFeature.propTypes = {
     
 };
 
 function TodoFeature(props) {
-    const todoList = [
+    const initTodoList = [
         {
             id: 1,
-        title: 'Eat'
+            title: 'Eattt',
+            status: 'completed',
         },
         {
             id: 2,
-        title: 'Sleep'
+            title: 'Sleep',
+            status: 'new',
         },
         {
             id: 3,
-        title: 'Game'
+            title: 'Gamet',
+            status: 'completed',
         },
         {
             id: 4,
-        title: 'Code'
+            title: 'Coded',
+            status: 'new',
         },
         {
             id: 5,
-        title: 'Travel'
+            title: 'Travel',
+            status: 'completed',
         },
-    ]
+    ];
+
+    const [todoList, setTodoList] = useState(initTodoList);
+    const handleTodoClick = (todo, idx) => {
+        // clone current array to the new one
+        const newTodoList = [...todoList]
+
+        console.log(todo, idx)
+
+        // toggle state
+        newTodoList[idx] = {
+            ...newTodoList[idx],
+            status: newTodoList[idx].status === 'new' ? 'completed' : 'new',
+        };
+
+        // upgrade todo list
+        setTodoList(newTodoList)
+    }
     return (
         <div>
             <h3>TodoList</h3>
-            <TodoList todoList={todoList}/>
+            <TodoList todoList={todoList} onTodoClick={handleTodoClick}/>
         </div>
     );
 }

a41a5d23f59076c5e27cf528e9ae6c2405ece8e7
bacnh7427 <bacnh7427@co-well.com.vn>
2023-04-13 10:59:30 +0700

    add Feature todo list using prop

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TodoList from './components/TodoList';
 
 TodoFeature.propTypes = {
     
 };
 
 function TodoFeature(props) {
+    const todoList = [
+        {
+            id: 1,
+        title: 'Eat'
+        },
+        {
+            id: 2,
+        title: 'Sleep'
+        },
+        {
+            id: 3,
+        title: 'Game'
+        },
+        {
+            id: 4,
+        title: 'Code'
+        },
+        {
+            id: 5,
+        title: 'Travel'
+        },
+    ]
     return (
         <div>
-            TodoFeature        
+            <h3>TodoList</h3>
+            <TodoList todoList={todoList}/>
         </div>
     );
 }

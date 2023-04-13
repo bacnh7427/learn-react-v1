21bbfb00d439c8b6b3aa708ed0e285d89330601a
bacnh7427 <bacnh7427@co-well.com.vn>
2023-04-13 13:13:52 +0700

    add album components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoFeature from './features/Todo';
+import AlbumFeature from './features/Album';
 
 function App() {
     return (
     <div className="App">
       <header className="App-header">
           <TodoFeature />
+          <AlbumFeature />
       </header>
     </div>
   );

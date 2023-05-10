03c51291bc7a00237f263e332c2e8d6e7add7226
bacng <nhb.fit.hut@gmail.com>
2023-05-10 23:47:23 +0700

    update

--- src/components/form-controls/InputField/index.jsx
+++ src/components/form-controls/InputField/index.jsx
@@ -13,18 +13,24 @@ InputField.propTypes = {
 
 function InputField(props) {
     const { form, name, label, disabled } = props;
+    const { errors, formState } = form;
+    // const hasError = formState.touched[name] && errors[name];
+    // console.log(errors[name], formState.touched[name]);
     return (
         <Controller
             name={name}
             control={form.control}
-            render = {({ field})=> (
-                <TextField
-                    fullWidth
-                    label={label}
+            as={TextField}
+            // render = {({ field})=> (
+            //     <TextField
+            fullWidth
+            label={label}
             disabled={disabled}
-            required
-                />
-            )}
+
+            //         error={!!hasError}
+            //         helperText={errors[name]?.message}
+            //     />
+            // )}
         />
     );
 }
